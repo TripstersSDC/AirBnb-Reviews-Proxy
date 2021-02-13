b2f2e1cef3332f456ecd79b403c09f8f37093e8c
Christina Santos <christina.santos823@gmail.com>
2021-02-12 19:36:01 -0800

    updated services ips

--- server/config/services.js
+++ server/config/services.js
@@ -2,17 +2,17 @@ module.exports = {
   service1: {
     api: '/rooms/:propertyId/booking',
     cal: '/rooms/:propertyId/booking/calendar',
-    url: 'http://52.12.194.253:3001',
+    url: 'http://54.244.174.42',
     bundle: 'bundle.js',
   },
   service2: {
     api: '/rooms/:propertyId/reviews',
-    url: 'http://54.67.82.0',
+    url: 'http://52.8.16.106',
     bundle: 'bundle.js',
   },
   service3: {
-    api: '/home/:id/photos',
-    url: 'http://3.19.213.23',
+    api: '/rooms/:propertyId/photos',
+    url: 'http://3.84.124.193',
     bundle: 'bundle.js',
   },
 };
